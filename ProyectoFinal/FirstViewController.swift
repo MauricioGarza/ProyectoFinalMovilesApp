28858af69ba08c26a8b404edaf9c9009f86d6ec6
Mauricio garza <mgarzaa96@gmail.com>
2018-03-13 12:17:11 -0600

    Initial Commit

---
+++ ProyectoFinal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ProyectoFinal
+//
+//  Created by Mauricio Garza on 3/13/18.
+//  Copyright © 2018 Mauricio Garza. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ProyectoFinal/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ProyectoFinal
+//
+//  Created by Mauricio Garza on 3/13/18.
+//  Copyright © 2018 Mauricio Garza. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
